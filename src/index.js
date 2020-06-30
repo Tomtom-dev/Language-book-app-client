de46d983cd9817d429a413d81d32e95339c204e9
Tomtom-dev <thomasheddadji0@gmail.com>
2020-06-30 14:53:09 +0200

    add root reducer and index in store

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {Provider} from "react-redux"
+import store from "./store"
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter as Router} from "react-router-dom"
 import 'bootstrap/dist/css/bootstrap.css'
@@ -9,14 +11,11 @@ import 'bootstrap/dist/css/bootstrap.css'
 
 ReactDOM.render(
   <Router>
-    <React.StrictMode>
+    <Provider store={store}>
       <App />
-    </React.StrictMode>
+    </Provider>
   </Router>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
