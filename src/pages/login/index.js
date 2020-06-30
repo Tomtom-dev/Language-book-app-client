fa873a1d33d88685125b9ac8be30a4cd5279665e
Tomtom-dev <thomasheddadji0@gmail.com>
2020-06-30 12:01:47 +0200

    add login page

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,9 +1,67 @@
-import React from 'react'
+import React,{useState} from 'react'
+import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import { useDispatch} from "react-redux";
+import { Link } from "react-router-dom";
+import './index.css'
 
 export default function Login() {
+
+    ;
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    function submitForm (event){
+       
+        event.preventDefault();
+        console.log(email,password);
+        
+      
+        setEmail("");
+        setPassword("");
+    }
+
     return (
         <div>
-            <h2>Login</h2>
+             <h2 style={{textAlign:"center"}}>Login</h2>
+            <Container>
+                <Form>
+                <Form.Group  controlId="formBasicEmail" className="form-size">
+                        
+                        <Form.Control 
+                        value={email}
+                        onChange={event=>setEmail(event.target.value)}
+                        type="email" 
+                        placeholder="Email"
+                        required
+                        />
+                            
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicPassword" className="form-size">
+                        <Form.Control 
+                        value={password}
+                        onChange={event=>setPassword(event.target.value)}
+                        type="password" 
+                        placeholder="Password"
+                        required
+                        />
+                    </Form.Group>
+
+                    <Form.Group >
+                        <Button variant="primary" type="submit" onClick={submitForm}>
+                            Log in
+                        </Button>
+                        <span> | Not registered ? </span>
+                        <Link to="/signup" style={{ textAlign: "center" }}>
+                            Sign Up
+                        </Link>
+                    </Form.Group>
+                </Form>
+            </Container>
+           
         </div>
     )
 }
