d76ce082ba69a4df55a158b3f4e8d9fa65fb0b93
SupriyaShekar123 <s.supriya82@gmail.com>
2020-06-30 15:01:26 +0200

    setup reducer for user

--- src/pages/my-books/index.js
+++ src/pages/my-books/index.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getUserBooks } from "../../store/user/action";
 
 export default function MyBooks() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch();
+  }, []);
+
   return (
     <div>
       <h2>My Books</h2>
