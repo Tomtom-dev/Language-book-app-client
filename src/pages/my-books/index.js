7eff4647ca69dcbeb8eb9f4808ae6ccb08737db7
SupriyaShekar123 <s.supriya82@gmail.com>
2020-06-30 13:51:02 +0200

    minor changes

--- src/pages/my-books/index.js
+++ src/pages/my-books/index.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 
 export default function MyBooks() {
-    return (
-        <div>
-            <h2>My Books</h2>
-        </div>
-    )
+  return (
+    <div>
+      <h2>My Books</h2>
+    </div>
+  );
 }
