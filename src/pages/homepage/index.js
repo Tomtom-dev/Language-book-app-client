10f6a509c8ab882d20f84e99f3d0fbb3d45fbbee
Tomtom-dev <thomasheddadji0@gmail.com>
2020-09-23 15:35:17 +0200

    add background video and styling for homepage

--- src/pages/homepage/index.js
+++ src/pages/homepage/index.js
@@ -1,21 +1,28 @@
 import React from 'react'
 import './index.css'
+import Biblio2 from "./Biblio22.mp4"
 
 export default function Homepage() {
     return (
-        <div >
-            
-            <img 
-            alt ="The library"
-            src="https://cdn.pixabay.com/photo/2016/03/26/22/21/books-1281581_960_720.jpg"
-            className="img-presentation"
-            ></img>
-           
-            <div className="text-presentation">
-                <h2> welcome to the international Open-library </h2>
-                <p>Are you looking for inspiring book in a selected language ?</p>
-                <p>find good references and exiting ebooks <a href="/findBook">here</a> </p>
+        <div>
+            <div>
+                <video 
+                autoPlay 
+                muted 
+                loop 
+                id="presentation video"
+                className="background_video">
+                <source src={Biblio2} type="video/mp4"/>
+                </video>
+                    <div className="text-presentation">
+                        <h2> welcome to the International open-library !</h2>
+                        <p>Are you looking for inspiring book in a selected language ?</p>
+                        <p>find good references and exiting ebooks <a href="/findBook">here</a> </p>
+                    </div>
             </div>
+           
+           
+            
         </div>
     )
 }
